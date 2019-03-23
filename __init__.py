498015e5e39aa2ec091d23a86c1af7fbc948f308
HotcakesSanderson <hotcakessanderson@gmail.com>
2019-03-23 12:17:45 -0500

    initialize(self) update

--- __init__.py
+++ __init__.py
@@ -9,14 +9,14 @@ class Meowcroft(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    def initializer(self):
-        self.audio_service = AudioService(self.bus)
+    def initialize(self):
+        self.audioservice = AudioService(self.bus)
 
     @intent_file_handler('meowcroft.intent')
     def handle_meowcroft(self, message):
-        self.audio_service.play('file:///opt/mycroft/skills/meowcroft-skill.hotcakessanderson/catmeow3.mp3')
-        self.audio_service.play('file:///opt/mycroft/skills/meowcroft-skill.hotcakessanderson/catmeow4.mp3')
-        self.audio_service.play('file:///opt/mycroft/skills/meowcroft-skill.hotcakessanderson/catmeow5.mp3')
+        self.audioservice.play('file:///opt/mycroft/skills/meowcroft-skill.hotcakessanderson/catmeow3.mp3')
+        self.audioservice.play('file:///opt/mycroft/skills/meowcroft-skill.hotcakessanderson/catmeow4.mp3')
+        self.audioservice.play('file:///opt/mycroft/skills/meowcroft-skill.hotcakessanderson/catmeow5.mp3')
 
 
 
