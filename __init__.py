052094fc2de4a0ddcb4daa2402786dc13b9da688
HotcakesSanderson <hotcakessanderson@gmail.com>
2019-03-23 11:16:57 -0500

    added audio

--- __init__.py
+++ __init__.py
@@ -1,15 +1,24 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.skills.audioservice import AudioService
+from mycroft.util.log import getLogger
+
+LOGGEr = getLogger(__name__)
 
 
 class Meowcroft(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    def initializer(self):
+        self.audioservice = AudioService(self.bus)
+
     @intent_file_handler('meowcroft.intent')
     def handle_meowcroft(self, message):
-        self.speak_dialog('meowcroft')
+        self.audioservice.play('file:///opt/mycroft/skills/meowcroft-skill.hotcakessanderson/catmeow3.mp3')
+        self.audioservice.play('file:///opt/mycroft/skills/meowcroft-skill.hotcakessanderson/catmeow4.mp3')
+        self.audioservice.play('file:///opt/mycroft/skills/meowcroft-skill.hotcakessanderson/catmeow5.mp3')
+
 
 
 def create_skill():
     return Meowcroft()
-
